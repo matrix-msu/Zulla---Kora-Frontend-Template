d6365a5eaebb910b4e794082b39564cb98d54627
kimeunh3 <kimeunh3@robbie.matrix.msu.edu>
2020-01-15 21:07:18 +0000

    Saving current changes on home page before robbie gets wiped out.

--- views/home.php
+++ views/home.php
@@ -1,22 +1,77 @@
 <div class="container main">
     <div class="container middlewrap">
-        <h1>Title of Project</h1>
-        <h3>Project tagline text. Lorem ipsum dolor sit amet, consectetur adipiscing.</h3>
-        <div class="container search">
+        <h1>Followed by some brief about information, or site tagline.</h1>
+        <p>Site Description. Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis nostrud exercitation ullamco laboris nisi <a>LEARN MORE</a></p>
+        <!-- <div class="container search">
             <form action="submit">
                 <input class="search-field" type="text" name="searchbar" placeholder="Start a Search for whatever it is that you may want in life"/>
                 <input class="search-submit" type="submit" alt="search.svg" />
             </form>
-        </div>
+        </div> -->
     </div>
 </div>
 <!--  description container  -->
-<div class="container about">
+<!-- <div class="container about">
     <div class="container aboutwrap">
         <p>Brief Project Description would go here. Lorem ipsum dolor sit amet, iusmod tempor incididunt ut labore et dolore magna aliqua. Pellentesque elit eget gravida sociis natoque penatibus et magnis. Dui accumsan sit amet nulla facilisi. Dignissim convallis aenean et tortor at risus viverra adipiscing. Tristique sollicitudin nibh sit amet. Volutpat ac tincidunt vitae semper. Donec adipiscing tristique risus nec feugiat. Pellentesque elit eget gravidasociis natoque penatibus et magnis. <a class="learn-more">Learn More</a></p>
     </div>
-</div>
+</div> -->
 <!--  Cards/Column container  -->
+<div class="card-slider featured-collections">
+    <div class="container collectionwrap">
+        <p>FEATURED COLLECTIONS<a>VIEW ALL</a></p>
+        <div class="cardwrap">
+            <div class="cardwrap2">
+                <ul class="cards">
+                    <li class="card card-collection">
+                      <a href="">
+                          <p class="card-resource">117 Resources</p>
+                          <h3 class="card-title">Collection Title</h3>
+                          <p class="card-desc">Lorem ipsum dolor sit amet.
+                          </p>
+                      </a>
+                    </li>
+                    <li class="card card-collection">
+                      <a href="">
+                          <p class="card-resource">117 Resources</p>
+                          <h3 class="card-title">Collection Title</h3>
+                          <p class="card-desc">Lorem ipsum dolor sit amet, iusmod tempor incididunt ut
+                            labore et dolore magna aliqua. Pellentesque elit eget
+                            gravida sociis natoque penatibus et magnis. Dui accumsan
+                            sit amet nulla facilisi. Dignissim convallis aenean et
+                            tortor at risus viverra adipiscing. Tristique sollicitudin
+                            nibh sit amet. Volutpat ac tincidunt vitae semper. 
+                            Donec adipiscing tristique risus nec feugiat.
+                            Pellentesque elit eget gravidasociis natoque penatibus et magnis.</p>
+                      </a>
+                    </li>
+                    <li class="card card-collection">
+                      <a href="">
+                          <p class="card-resource">117 Resources</p>
+                          <h3 class="card-title">Collection Title</h3>
+                          <p class="card-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                          </p>
+                      </a>
+                    </li>
+                    <li class="card card-collection">
+                      <a href="">
+                          <p class="card-resource">117 Resources</p>
+                          <h3 class="card-title">Collection Title</h3>
+                          <p class="card-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                          </p>
+                      </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+
+
 <div class="container column">
     <div class="container cardwrap">
         <h2 class="column-header">Cards/ 3 column layout</h2>
@@ -93,4 +148,11 @@
             </a>
         </div>
     </div>
+</div>
+
+<div class="container search">
+    <form action="submit">
+        <input class="search-field" type="text" name="searchbar" placeholder="Start a Search for whatever it is that you may want in life"/>
+        <input class="search-submit" type="submit" alt="search.svg" />
+    </form>
 </div>
